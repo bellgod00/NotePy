852e82fd8b47d415789e27df72f439fd16937e35
bellgod00 <bellgod@curocom.com>
2025-12-01 14:38:57 +0900

    First Commit

---
+++ sample/fortest1.py
@@ -0,0 +1,17 @@
+# fortest1.py
+marks = [90, 25, 67, 45, 80]   # 학생들의 시험 점수 리스트
+
+number = 0   # 학생에게 붙여 줄 번호
+for mark in marks:   # 90, 25, 67, 45, 80을 순서대로 mark에 대입
+    number = number +1 
+    if mark >= 60: 
+        print("%d번 학생은 합격입니다." % number)
+    else: 
+        print("%d번 학생은 불합격입니다." % number)
+
+
+add = 0 
+for i in range(1, 11): 
+  print(f"test1 : {add} {add+i}")
+  add = add + i 
+
